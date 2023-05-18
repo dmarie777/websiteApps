7182674a9bb59e4f13a63de61709082389309419
dmarie777 <deimarie777@gmail.com>
2023-05-18 09:34:27 -0500

    Bugfix in buttons btn-next and btn-prev in js

--- CooktailApp/js/main.js
+++ CooktailApp/js/main.js
@@ -1,42 +1,53 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
-let count = 0;
+
 document.querySelector('button').addEventListener('click', getDrink)
 
-
-
-
 function getDrink() {
-    //changes the hidden class in the arrow buttons
-    document.querySelector('.btn-next').classList.toggle('hidden')
-    document.querySelector('.btn-prev').classList.toggle('hidden')
-
+    //Show the arrows 
+    document.querySelector('.btn-next').classList.add('hidden')
+    document.querySelector('.btn-prev').classList.add('hidden')
+    //Get the drink inserted in ther input
     let drinks = document.querySelector('input').value.toLowerCase();
     const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinks}`;
     console.log(url);
-    ///we need to use https to get working, else it doesn't
+    let count = 0;
+        ///we need to use https to get working, else it doesn't
     const ul = document.querySelector('ul')
-    const body = document.querySelector('body')
     const p = document.querySelector('p');
-    const h3 = document.querySelector('h3')
-
+    const h3 = document.querySelector('h3');
+    //extract the data using the url for the drink
     fetch (url) 
         .then(res => res.json())
         .then(data => {
-            console.log(`count1: ${count}`);
-            //console.log(data);//object
+            console.log(`Initial count: ${count}`);
+                //console.log(data);//object
             console.log(data.drinks);//array with objects
-            // let drink =data.drinks [0] 
             let arrDrinks = data.drinks;
-            //this part of the code do the carousel, when you fished all the drinks you got the fist one again an then the second etc.///
-
+            //Insert the content for drink in index 0
+            insertContent()
             //Button next interactions
             document.querySelector('.btn-next').addEventListener('click', nextCocktail)
             function nextCocktail () {
-                //this codes runs when the loop reaches the end!!!
+                count ++;
                 showFinalMessage(arrDrinks.length)
                 removeIngredients()
-                 //this part of the code makes the counter re start and the drinks starts again!
-                 if (count>=arrDrinks.length) {
+                insertContent()
+                
+            }
+            document.querySelector('.btn-prev').addEventListener('click', prevCocktail)
+            function prevCocktail () {
+                count --;
+                showFinalMessage(-1)
+                removeIngredients()
+                insertContent()
+            }
+            
+            ///function for writting 
+            function insertContent() {
+                if (count<0) {
+                    count =arrDrinks.length-1
+                    console.log(`count3: ${count}`);
+                } else if (count>=arrDrinks.length) {
                     count =0
                     console.log(`count3: ${count}`);
                 } else {  
@@ -45,30 +56,9 @@ function getDrink() {
                     document.querySelector('h2').innerText = arrDrinks[count].strDrink
                     getIngredients(count)
                     getInstructions(count)
-                    count ++;
-                    console.log(`counting after Adding: ${count}`);
+                    console.log(`count: ${count}`);
                 }
             }
-            document.querySelector('.btn-prev').addEventListener('click', prevCocktail)
-            function prevCocktail () {
-                showFinalMessage(-1)
-                removeIngredients()
-                 //this part of the code makes the counter re start and the drinks starts again!
-                if (count<0) {
-                    count =arrDrinks.length-1
-                    console.log(`count3: ${count}`);
-                } else {  
-                    ///this code overwrites the existing data for img and h2 each iteration///
-                    document.querySelector("img").src = arrDrinks[count].strDrinkThumb
-                    document.querySelector('h2').innerText = arrDrinks[count].strDrink
-                    getIngredients(count)
-                    getInstructions(count)
-                    count --;
-                    console.log(`countAfterSubstracting: ${count}`);
-                }
-            }
-            
-
             ///code for removing every li that has been created in the previous iteration!!! USED IN INGREDIENTS///
             function removeIngredients() {
                 const liArr = document.querySelectorAll('li') //this is an array
